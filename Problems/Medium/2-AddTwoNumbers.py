36fbac70c4e8a2606b81f58c4b30aacb6aa59a92
Bravotty <tychen@m.scnu.edu.cn>
2019-03-18 11:03:22 +0800

    Create 2-AddTwoNumbers.py

---
+++ Problems/Medium/2-AddTwoNumbers.py
@@ -0,0 +1,56 @@
+# coding=utf-8
+# python3
+# https://leetcode.com/problems/add-two-numbers/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 == None:
+            return l2
+        if l2 == None:
+            return l1
+        # ini----
+        result = ListNode(0)
+        p = result
+        flag = 0
+        # same add
+        while l1 and l2:
+            sum = l1.val + l2.val + flag
+            # 
+            flag = 0
+            if sum >= 10:
+                sum -= 10
+                flag = 1
+            p.next = ListNode(sum)
+            l1 = l1.next
+            l2 = l2.next
+            p = p.next
+        if l1:
+            while l1:
+                sum = l1.val + flag
+                flag = 0
+                if sum >= 10:
+                    sum -= 10
+                    flag = 1
+                p.next = ListNode(sum)
+                l1 = l1.next
+                p = p.next
+        if l2:
+            while l2:
+                sum = l2.val + flag
+                flag = 0
+                if sum >= 10:
+                    sum -= 10
+                    flag = 1
+                p.next = ListNode(sum)
+                l2 = l2.next
+                p = p.next
+        if flag:
+            p.next = ListNode(flag)
+            p = p.next
+        return result.next
